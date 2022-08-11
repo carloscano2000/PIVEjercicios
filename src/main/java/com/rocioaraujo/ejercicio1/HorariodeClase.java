6820ec2e533ffb6aec8ed9b8e9876285eec11648
CDE-LAB3-PC32 <CDE-LAB3-PC32@DESKTOP-E8A1TGI>
2022-08-10 21:25:05 -0400

    Inicio de aplicación

---
+++ src/main/java/com/rocioaraujo/ejercicio1/HorariodeClase.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rocioaraujo.ejercicio1;
+
+/**
+ *
+ * @author CDE-LAB3-PC32
+ */
+public class HorariodeClase {
+    public static void main(String[] args) {
+        System.out.println("lUNES\t\tMartes\t\tMierc.\t\tJueves\t\tViernes");
+        System.out.println("=====\t\t ======\t\t =====\t\t ======\t\t ======");
+        System.out.println("Proy.inf\tArq Inf\t\tProg IV\t\tAud Sist\tIng Soft\t");
+        
+        
+    }
+    
+}
